1a81b54a5c3b2d034ff73504c58b9f6e9c1ef0ed
muminf <fayzievmuminjon@gmail.com>
2021-09-15 20:04:08 +0500

    add types.go

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,29 @@
+package types
+
+// Amount of money in minimum units (diram, kopeyka, cent...)
+type Money int64
+
+// Code of currency
+type Currency string
+
+// Codes of currency
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// Number of card
+type PAN string
+
+// Info about payment card
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
+}
